665cc30d402b5afdf45e30229d152288eae235d5
Eugene Kalinin <ekalinin@users.noreply.github.com>
2020-04-10 00:43:05 +0300

    Update basic-setup.sh
    Fix DNS resolving in some cases + apt cache update

--- basic-setup.sh
+++ basic-setup.sh
@@ -2,9 +2,13 @@
 
 hostname=$(hostname)
 
+# fix DNS resolving in some cases
+echo "nameserver 8.8.8.8" > /etc/resolv.conf
+
 # install gobgp
 cp /tmp/gobgpd.conf /etc
-apt-get install gobgpd
+apt-get update -y
+apt-get install gobgpd -y
 
 whoami
 printenv
