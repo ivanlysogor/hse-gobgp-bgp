86825aa3ac69780a86581dfb426f734899f648b2
Eugene Kalinin <ekalinin@users.noreply.github.com>
2020-04-10 11:08:14 +0300

    Update basic-setup.sh
    Add traceroute installation

--- basic-setup.sh
+++ basic-setup.sh
@@ -5,10 +5,15 @@ hostname=$(hostname)
 # fix DNS resolving in some cases
 echo "nameserver 8.8.8.8" > /etc/resolv.conf
 
+# update apt cache
+apt-get update -y
+
 # install gobgp
 cp /tmp/gobgpd.conf /etc
-apt-get update -y
 apt-get install gobgpd -y
+
+# install traceroute
+apt-get install traceroute -y
 
 whoami
 printenv
